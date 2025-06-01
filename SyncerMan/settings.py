21f1341ce9b81989b4ff56c3f0eb3f3cf494f7aa
davoodya <davoodya40@gmail.com>
2025-06-01 17:32:58 +0330

    Change settings for testing on Real Environments

--- SyncerMan/settings.py
+++ SyncerMan/settings.py
@@ -1,6 +1,6 @@
-"""
+""" Tool Notes:
 This tool written by Yakuza.D; Hello Ninja, Use it and Enjoy...
-Notes:
+For Using tool:
     1. You should define Allow Firewall Specially for windows (Incoming on 65432, 65433)
     2. If on Linux server using Firewall, you should define allow rule for port 65432 and 65433
 
@@ -9,14 +9,14 @@ Notes:
 """ Section 0: Define Global Variables """
 
 """ Variables for Client (Windows) """
-SERVER_IP = "192.168.10.78"  # Linux Server IP address
+SERVER_IP = "192.168.1.102"  # Linux Server IP address
 
 SERVER_PORT = 65432 # Linux Server Listening Port
 # Note: SERVER_PORT should be the same as server side in start_receive_server(port=65432)
 
 
 """ Variables for Server (Linux) """
-WINDOWS_CLIENT_IP = "192.168.10.100" # Windows Client IP address
+WINDOWS_CLIENT_IP = "192.168.1.103" # Windows Client IP address
 
 
 """ Variables for Both Client & Server Sides (Windows and Linux) """
