c11ba948f1395c90b27ff8dbc892aa83e0e2a797
davoodya <davoodya40@gmail.com>
2025-06-02 19:12:56 +0330

    Change settings.py for testing on Localhost

--- SyncerMan/settings.py
+++ SyncerMan/settings.py
@@ -9,15 +9,15 @@ For Using tool:
 """ Section 0: Define Global Variables """
 
 """ Variables for Client (Windows) """
-SERVER_IP = "192.168.1.102"  # Linux Server IP address
-#SERVER_IP = "127.0.0.1"
+#SERVER_IP = "192.168.1.102"  # Linux Server IP address
+SERVER_IP = "127.0.0.1"
 SERVER_PORT = 65432 # Linux Server Listening Port
 # Note: SERVER_PORT should be the same as server side in start_receive_server(port=65432)
 
 
 """ Variables for Server (Linux) """
-WINDOWS_CLIENT_IP = "192.168.1.103" # Windows Client IP address
-#WINDOWS_CLIENT_IP = "127.0.0.1"
+#WINDOWS_CLIENT_IP = "192.168.1.103" # Windows Client IP address
+WINDOWS_CLIENT_IP = "127.0.0.1"
 
 """ Variables for Both Client & Server Sides (Windows and Linux) """
 CLIENT_RECEIVE_PORT = 65433 # Windows Client Port for receiving from Windows (Bidirectional Synchronization)
